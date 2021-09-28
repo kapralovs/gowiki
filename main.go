4e6caedddc43c421e50ed08c08927bbf47eac0c1
kapralovs <ser-kapralov@yandex.ru>
2021-09-28 13:25:11 +0300

    Add saveHandler, template caching

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
+
+var templates:=template.Must(template.ParsrFiles("edit.html","view.html"))
+
+var validPath=regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 type Page struct {
 	Title string
@@ -27,23 +32,59 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	title,err:=getTitle(w,r)
+	if err!=nil{
+		return
+	}
+	p, err:= loadPage(title)
+	if err!=nil{
+		http.Redirect(w,r,"/edit/"+title,http.StatusFound)
+		return
+	}
 	renderTemplate(w, "view", p)
 }
 
+func saveHandler(w http.ResponseWriter,r *http.Request){
+	title,err:=getTitle(w,r)
+	if err!=nil{
+		return
+	}
+	body:=r.FormValue("body")
+	p:=&Page{Title:title,Body:[]body(body)}
+	err:=p.save()
+	if err!=nil{
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w,r,"/view/"+title, http.StatusFound)
+}
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
+	err:=templates.ExecuteTemplate(w,tmpl+".html",p)
+	if err!=nil{
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+	}
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title,err := getTitle(w,r)
+	if err!=nil{
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
 	renderTemplate(w, "edit", p)
+}
+
+func getTitle(w http.ResponseWriter,r *http.Request) (string,error){
+	m:=validPath.FindStringSubmatch(r.URL.Path)
+	if m==nil{
+		http.NotFound(w,r)
+		return "",errors.New("invalid Page Title")
+	}
+	return m[2],nil	//Заголовок является вторым подвыражением
 }
 
 func main() {
